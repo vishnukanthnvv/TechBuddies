125753dd0dfa3e268dc17fb8cc258ef1a8a6488e
vishnukanthnvv <vishnukanth.n@gmail.com>
2026-01-30 18:03:32 +0530

    Sample request handlers for user API  get, post, patch, del calls

--- src/app.js
+++ src/app.js
@@ -2,14 +2,33 @@ const express = require("express");
 
 const app = express();
 
+// .get will match only the get API call with respective matching route
 
+app.get("/user", (req, res) => {
+    res.send({
+        "firstName": "Vishnu Kanth",
+        "lastName": "Nalamati",
+        "dob": "18/08/1988",
+        "city": "Vizag"
+    });
+});
+
+app.post("/user", (req, res) => {
+    res.send("User data saved succesfully");
+})
+
+app.patch("/user", (req, res) => {
+    res.send("User details updated successfully");
+});
+
+app.delete("/user", (req, res) => {
+    res.send("User deleted successfully");
+});
+
+// .use will match all http method calls  matching the route like get, post, put
 app.use("/test", (req, res) => {
     res.send("Welcome to test API");
 });
-
-app.use("/hello", (req, res) => {
-    res.send("Hello from Vishnu");
-})
 
 app.use("/", (req, res) => {
     res.send("Welcome to Dashboard");
