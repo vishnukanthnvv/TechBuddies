2b95c6c8894fd4d890ba640c384a38561e05c631
vishnukanthnvv <vishnukanth.n@gmail.com>
2026-02-05 03:22:12 +0530

    implement /logout API

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -76,4 +76,12 @@ authRouter.post("/login", async (req, res) => {
     }
 });
 
+authRouter.post("/logout", async (req, res) => {
+    res
+    .cookie("token", null, {
+        expires: new Date(Date.now())
+    })
+    .send("Logout successfull")
+})
+
 module.exports = authRouter;
