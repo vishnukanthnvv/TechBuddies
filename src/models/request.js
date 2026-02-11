3425ce0169a5e58a4bb6c513de56b8af298c65d8
vishnukanthnvv <vishnukanth.n@gmail.com>
2026-02-11 22:44:04 +0530

    GET user/connections API along with data validation

--- src/models/request.js
+++ src/models/request.js
@@ -8,7 +8,8 @@ const connectionRequestSchema = new mongoose.Schema({
     },
     toUserId: {
         type: mongoose.Schema.Types.ObjectId,
-        required: true
+        required: true,
+        ref: "User"
     },
     status: {
         type: String,
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 
 const { authUser } = require("../middlewares/auth"); 
 const connectionRequestModel = require("../models/request");
+const USER_SAFE_DATA = "firstName lastName photoUrl age gender skills about";
 
 userRouter.get("/user/requests/received", authUser, async (req, res) => {
     try{
@@ -11,7 +12,7 @@ userRouter.get("/user/requests/received", authUser, async (req, res) => {
         const connectionRequests = await connectionRequestModel.find({
             toUserId: loggedInUserId,
             status: "interested"
-        }).populate("fromUserId", "firstName lastName photoUrl age gender skills about");
+        }).populate("fromUserId", USER_SAFE_DATA);
     
         res.json({
             message: "Data fetched successfully",
@@ -20,6 +21,34 @@ userRouter.get("/user/requests/received", authUser, async (req, res) => {
     }
     catch(err){
         res.status(400).send("Error: "+err.message);
+    }
+});
+
+userRouter.get("/user/connections", authUser, async (req, res) => {
+    try{
+        const loggedInUserId = req.user._id;
+
+        const connectionRequests = await connectionRequestModel.find({
+            $or: [
+                { fromUserId: loggedInUserId, status: "accepted" },
+                { toUserId: loggedInUserId, status: "accepted" }
+            ]
+        })
+        .populate("fromUserId", USER_SAFE_DATA)
+        .populate("toUserId", USER_SAFE_DATA)
+
+        const data = connectionRequests.map(conn => {
+            if(conn.fromUserId._id.toString() === loggedInUserId.toString()){
+                return conn.toUserId;
+            }
+            return conn.fromUserId;
+        });
+
+        res.json({data});
+    }
+    catch(err){
+        res.status(400)
+        .send("Error: "+err.message);
     }
 })
 
